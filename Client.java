00a31defbf6316ee6d25e3bda7df421671ba6793
antoinejessel <128071878+antoinejessel@users.noreply.github.com>
2025-04-20 21:39:41 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -25,7 +25,7 @@ public class Client {
     // Getters / Setters
     // ------------------------------------------------
     public int getId() {
-        return this.id;
+        return this.nom;
 
     }
     public void setId(int id) {}
